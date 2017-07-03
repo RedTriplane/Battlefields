1d9789db910bcdcef3c94f65dc77dfc2f4a3378d
J Fixby <github@jfixby.com>
2017-07-03 12:00:09 +0200

    bt api

--- battlefields-p18-red/src/com/jfixby/util/patch18/red/fields/afield/WallsFieldSchematic.java
+++
@@ -1,6 +0,0 @@
-
-package com.jfixby.util.patch18.red.fields.afield;
-
-public class WallsFieldSchematic {
-
-}
--- battlefields-terrain-p18-red/src/com/jfixby/r3/ext/p18t/red/RedP18LandscapeListener.java
+++
@@ -1,6 +0,0 @@
-
-package com.jfixby.r3.ext.p18t.red;
-
-public class RedP18LandscapeListener {
-
-}
